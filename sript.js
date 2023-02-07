18b0ed617d4fc2a5cab09dd2c6b4326a85b4e2c9
Edina05 <ets.deliedina@gmail.com>
2023-02-07 15:52:40 +0100

    uzenet

---
+++ sript.js
@@ -0,0 +1,13 @@
+console.dir(document);
+//document.title="elso nev"
+//document.log(document.head)
+// documenty.all[8].textContent = "Masik cim"
+//console.log(document.getElementById("main_titile"));
+//document.getElementById("main_title").textContent = "harmadik cim";
+//let cim = document.getElementById("main_title");
+//cim.textContent = "negyedik cim";
+for (let i = 0; i < items.lenght; i++) {
+  if (i % 2 == 0) {
+    item[i].style.backgraundColor = "lightgrey";
+  }
+}
