254a241ab910cd65a04fb024d4e4beb6dd2fafa6
Your Name <you@example.com>
2023-02-21 15:53:48 +0100

    kecske

--- sript.js
+++ sript.js
@@ -6,8 +6,25 @@ console.dir(document);
 //document.getElementById("main_title").textContent = "harmadik cim";
 //let cim = document.getElementById("main_title");
 //cim.textContent = "negyedik cim";
-for (let i = 0; i < items.lenght; i++) {
+/*for (let i = 0; i < items.lenght; i++) {
   if (i % 2 == 0) {
     item[i].style.backgraundColor = "lightgrey";
   }
 }
+let itemList = document.querySelector("#list");
+console.log(itemList.parentElement.parentElement);
+//ChildNode
+console.log(itemList.childNode);
+console.log(itemList.children);
+//firstChild
+console.log(itemList.lastChild);
+console.log((itemList.children[3].style.backgroundColor = "green"));
+
+console.log(itemList.nextElementSibling);
+itemList.nextElementSibling.style.backgraundColor = "purple";
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.backgraundColor = "yellow";
+*/
+let masodikP = document.querySelector("#masodik p");
+
+console.log(masodikP.nextElementSibling);
